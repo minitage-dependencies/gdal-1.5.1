b073293a5fd749af6dc01ebe97317aec2d61faf3
kiorky <devnull@localhost>
2008-05-15 23:23:14 +0200

    refactoring function

--- hooks/gdal.py
+++ hooks/gdal.py
@@ -8,7 +8,7 @@ os_ldflags=''
 uname=os.uname()[0]
 if uname == 'Darwin':
     os_ldflags=' -mmacosx-version-min=10.5.0'
- 
+
 log = logging.getLogger('GDAL hook')
 def substitute(filename, search_re, replacement):
     """Substitutes text within the contents of ``filename`` matching
@@ -17,7 +17,7 @@ def substitute(filename, search_re, replacement):
     search = re.compile(search_re, re.MULTILINE)
     text = open(filename).read()
     text = replacement.join(search.split(text))
-    
+
     newfilename = '%s%s' % (filename, '.~new')
     newfile = open(newfilename, 'w')
     newfile.write(text)
@@ -37,15 +37,13 @@ def post_make(options, buildout):
     substitute('setup.py',
                'extra_link_args=EXTRA_LINK_ARGS,',
                'extra_link_args=EXTRA_LINK_ARGS, runtime_library_dirs=%s' % str(rpath))
-    
+
     cmd = '%s setup.py install' % buildout[buildout['buildout']['python']]['executable']
     log.info('Installing GDAL python bindings')
 
     if os.system(cmd):
         log.error('Error executing command: %s' % cmd)
         raise zc.buildout.UserError('System error')
-
-
 
 def appendEnvVar(env,var,sep=":",before=True):
     """ append text to a environnement variable
@@ -68,6 +66,5 @@ def getgdalenv(options,buildout):
     # see http://trac.osgeo.org/gdal/wiki/FAQInstallationAndBuilding#HowcaniaddparticularLDFLAGSwithGDAL1.5 (little hack (kiorky))
     os.environ['LNK_FLAGS'] = os.environ['LDFLAGS']
     os.environ['LIBS'] = os.environ['LDFLAGS']
-
 
 # vim:set ts=4 sts=4 et  :
